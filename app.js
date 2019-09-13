eefec4f4967300f4d73647902cf82667d1e48d05
sanjayparidar <sanjaypatidar402@gmail.com>
2019-09-13 16:34:53 +0530

    9 agust

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-hello
+var express=require("express");
+var app=express();
+
+var bodyparser=require('body-parser');
+app.use(bodyparser());
+app.get('/login',function(req,res){
+    res.send("hello server")
+})
+
+app.listen(3000,function(){
+    console.log(" holisol server")
+});
